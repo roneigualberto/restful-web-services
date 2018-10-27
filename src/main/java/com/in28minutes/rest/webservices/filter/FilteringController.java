1ba504907294e3c3cc126149ad59fb01829b989f
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-10-27 18:11:54 -0400

    Implementing Static Filtering for RESTful Service

---
+++ src/main/java/com/in28minutes/rest/webservices/filter/FilteringController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.rest.webservices.filter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+
+	@GetMapping("/filtering")
+	public SomeBean retrieveSomeBean() {
+
+		return new SomeBean("v1", "v2", "v3");
+	}
+	
+	@GetMapping("/filtering-list")
+	public List<SomeBean> retrieveSomeBeanList() {
+
+		return Arrays.asList(new SomeBean("v1", "v2", "v3"));
+	}
+
+}
