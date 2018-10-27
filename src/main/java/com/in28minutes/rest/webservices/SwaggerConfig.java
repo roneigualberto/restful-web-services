1a8531c162d93ca9a741c261a17cb309942eba87
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-10-27 17:49:51 -0400

    Monitoring APIs with Spring Boot Actuator

--- src/main/java/com/in28minutes/rest/webservices/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/SwaggerConfig.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
