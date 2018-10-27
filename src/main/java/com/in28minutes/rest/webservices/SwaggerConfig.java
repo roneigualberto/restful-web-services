49bfcd7c45ed47fc9cc870bd34c57239de78edb4
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-10-27 17:34:07 -0400

    Enhancing Swagger Documentation with Custom Annotations

--- src/main/java/com/in28minutes/rest/webservices/SwaggerConfig.java
+++ src/main/java/com/in28minutes/rest/webservices/SwaggerConfig.java
@@ -1,8 +1,16 @@
 package com.in28minutes.rest.webservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,10 +19,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+	public static final Contact DEFAULT_CONTACT = new Contact("Ranga Karanam", "http://www.in28minutes.com",
+			"in28@gmail.com");
+
+	private static final ApiInfo DEFAULT_API_INFO = new ApiInfoBuilder()
+			.title("Awesome API Title")
+				.description("Awesome API Documentation")
+				.version("1.0")
+				.termsOfServiceUrl("urn:tos")
+				.contact(DEFAULT_CONTACT)
+				.license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licences/LICENSE-2.0")
+				.build();
+
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES =
+
+			new HashSet<>(Arrays
+					.asList("application/json", "application/xml"));
+
 	@Bean
 	public Docket api() {
 
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+					.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+					.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
 
 }
